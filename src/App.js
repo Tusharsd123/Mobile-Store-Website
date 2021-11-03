76cc055bdad115e5fda472e2043c6701649e5878
Tushar Diwakar <www.tushardiwakar@gmail.com>
2021-11-03 23:44:16 +0530

    added carousel using react-bootstrap

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import './App.css';
 import PreNavbar from './components/PreNavbar';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router } from "react-router-dom"
+import Slider from './components/Slider'
+import data from './data/data.json'
+import Offers from './components/Offers'
 
 function App() {
   return (
     <Router>
        <PreNavbar />
       <Navbar />
+      <Slider start={data.banner.start} />
+      <Offers offers={data.offer} />
     </Router>
   );
 }
